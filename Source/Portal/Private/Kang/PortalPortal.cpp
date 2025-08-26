1d28237631ef4d66cfa75a511711ec70496623ec
kleo19 <kleo19@naver.com>
2025-08-26 17:07:41 +0900

    CPP_implement-1stTry

--- Source/Portal/Private/Kang/PortalPortal.cpp
+++ Source/Portal/Private/Kang/PortalPortal.cpp
@@ -3,13 +3,101 @@
 
 #include "Kang/PortalPortal.h"
 
+
 // Sets default values
 APortalPortal::APortalPortal()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	bIsActive = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	RootComponent->Mobility = EComponentMobility::Static;
+
+	PortalRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("PortalRootComponent"));
+	PortalRootComponent->SetupAttachment(GetRootComponent());
+	PortalRootComponent->SetRelativeLocation( FVector(0.0f, 0.0f, 0.0f) );
+	PortalRootComponent->SetRelativeRotation( FRotator(0.0f, 0.0f, 0.0f) );
+	PortalRootComponent->Mobility = EComponentMobility::Movable;
+}
+
+bool APortalPortal::IsActive()
+{
+	return bIsActive;
+}
+
+void APortalPortal::SetActive( bool NewActive )
+{
+	bIsActive = NewActive;
+}
+
+void APortalPortal::ClearRTT_Implementation()
+{
 
 }
+
+void APortalPortal::SetRTT_Implementation( UTexture* RenderTexture )
+{
+
+}
+
+void APortalPortal::ForceTick_Implementation()
+{
+
+}
+
+AActor* APortalPortal::GetTarget()
+{
+	return Target;
+}
+
+void APortalPortal::SetTarget(AActor* NewTarget)
+{
+	Target = NewTarget;
+}
+
+bool APortalPortal::IsPointInFrontOfPortal( FVector Point, FVector PortalLocation, FVector PortalNormal )
+{
+	FPlane PortalPlane  = FPlane( PortalLocation, PortalNormal );
+	float PortalDot = PortalPlane.PlaneDot( Point );
+
+	//If < 0 means we are behind the Plane
+	return ( PortalDot >= 0 );
+}
+
+bool APortalPortal::IsPointCrossingPortal( FVector Point, FVector PortalLocation, FVector PortalNormal )
+{
+	FVector IntersectionPoint;
+	FPlane PortalPlane  = FPlane( PortalLocation, PortalNormal );
+	float PortalDot = PortalPlane.PlaneDot( Point );
+	bool IsCrossing = false;
+	bool IsInFront = PortalDot >= 0;
+
+	bool IsIntersect = FMath::SegmentPlaneIntersection(  LastPosition,
+															Point,
+															PortalPlane,
+															IntersectionPoint );
+
+	// Did we intersect the portal since last Location ?
+	// If yes, check the direction : crossing forward means we were in front and now at the back
+	// If we crossed backward, ignore it
+	if( IsIntersect && !IsInFront && LastInFront )
+	{
+		IsCrossing = true;
+	}
+
+	// Store values for next check
+	LastInFront = IsInFront;
+	LastPosition = Point;
+
+	return IsCrossing;
+}
+
+
+
+
+
+
 
 // Called when the game starts or when spawned
 void APortalPortal::BeginPlay()
--- Source/Portal/Public/Kang/PortalPortal.h
+++ Source/Portal/Public/Kang/PortalPortal.h
@@ -11,16 +11,64 @@ class PORTAL_API APortalPortal : public AActor
 {
 	GENERATED_BODY()
 	
-public:	
-	// Sets default values for this actor's properties
-	APortalPortal();
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
+	// Sets default values for this actor's properties
+	APortalPortal();
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	//Status of the Portal (being visualized by the player or not)
+	UFUNCTION(BlueprintPure)
+	bool IsActive();
+
+	UFUNCTION(BlueprintCallable)
+	void SetActive( bool NewActive );
+
+	//Render target to use to display the portal
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void ClearRTT();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void SetRTT( UTexture* RenderTexture );
+
+	UFUNCTION(BlueprintNativeEvent)
+	void ForceTick();
+
+	//Target of where the portal is looking
+	UFUNCTION(BlueprintPure)
+	AActor* GetTarget();
+
+	UFUNCTION(BlueprintCallable)
+	void SetTarget( AActor* NewTarget );
+
+	//Helpers
+	UFUNCTION(BlueprintCallable)
+	bool IsPointInFrontOfPortal( FVector Point, FVector PortalLocation, FVector PortalNormal );
+
+	UFUNCTION(BlueprintCallable)
+	bool IsPointCrossingPortal( FVector Point, FVector PortalLocation, FVector PortalNormal );
+
+	/*UFUNCTION(BlueprintCallable)
+	void TeleportActor( AActor* ActorToTeleport );*/
+	
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	USceneComponent* PortalRootComponent;
+
+private:
+	bool bIsActive;
+
+	AActor* Target;
+
+	//Used for Tracking movement of a point
+	FVector LastPosition;
+	bool    LastInFront;
 };
+
+
+
